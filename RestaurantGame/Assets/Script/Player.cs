070b98188e984d1ee3181cf7f85a6cbeae84f2f8
RaphaelaAttard <raphaela.attard.h63490@mcast.edu.mt>
2026-03-23 00:23:55 +0100

    Created unity project
    Added:
    -Player input
    - collision
    - kitchen assets

---
+++ RestaurantGame/Assets/Script/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+       
+    }
+}
